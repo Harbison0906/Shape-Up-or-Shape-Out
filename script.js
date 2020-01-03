578ceff7cbc442a32145ee59422c5b86758a3a6f
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-01-02 21:41:09 -0600

    started setting up classes for different shapes

---
+++ script.js
@@ -0,0 +1,27 @@
+class Shape {
+
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+
+  }
+}
+
+class Triangle extends Shape {
+  constructor(height) {
+
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(width, height) {
+
+  }
+}
+
+class Square extends Shape {
+  constructor(sideLength) {
+
+  }
+}
