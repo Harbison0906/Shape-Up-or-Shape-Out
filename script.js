2a8b48701bc1a962e10cd46e0f5652a3230b5157
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-01-07 20:46:45 -0600

    more work on building Shapes class; figuring out random position helper method

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+function randomVal(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 class Shape {
   constructor(width, height) {
+    this.width = width;
+    this.height = height;
     this.div = $('<div></div>');
     this.div.addClass('shape');
+    this.randomPos();
+    // this.div.style.left = `${x}px`;
+    // this.div.style.top = `${y}px`;
+  }
+
+  randomPos () {
+    this.position.style(`position: absolute, left: ${randomVal(0, 600)}, top: ${randomVal(0, 600)}`);
   }
 }
 
