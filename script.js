cee162463f72d64cea62594b1e8cf329f9b37f03
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-01-08 21:41:06 -0600

    added to Shape class and added to Rectangle and Square class.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-function randomVal(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+
 
 class Shape {
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.div = $('<div></div>');
-    this.div.addClass('shape');
+    this.div = $('<div class="shape"></div>');
     this.randomPos();
-    // this.div.style.left = `${x}px`;
-    // this.div.style.top = `${y}px`;
+    this.div.dblclick(() => this.div.remove());
+    this.div.click(() => this.describe());
   }
-
-  randomPos () {
-    this.position.style(`position: absolute, left: ${randomVal(0, 600)}, top: ${randomVal(0, 600)}`);
+  randomVal(min, max) {
+    return Math.floor(Math.random() * (max - min));
+  }
+  randomPos() {
+    this.div.css({
+      top: `${this.randomVal(this.height, 600)}px`,
+      left: `${this.randomVal(this.width, 600)}px`,
+      height: this.height,
+      width: this.width,
+    });
+    $('#shapeBoard').append(this.div);
+  }
+  describe() {
+    $('#describe-height').val(this.height);
   }
 }
 
@@ -32,12 +40,16 @@ class Triangle extends Shape {
 
 class Rectangle extends Shape {
   constructor(width, height) {
-
+    super(width, height)
+    this.div.attr('id', 'rectangle');
   }
 }
+new Rectangle(300, 150);
 
 class Square extends Shape {
   constructor(sideLength) {
-
+    super(sideLength, sideLength);
+    this.div.attr('id', 'square');
   }
 }
+new Square(150);
