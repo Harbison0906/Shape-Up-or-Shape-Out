17a535d389408538723038313131f820cdd71272
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-01-11 15:32:36 -0600

    added to and completed child classes for Square and Rectangle

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ class Shape {
     this.randomPos();
     this.div.dblclick(() => this.div.remove());
     this.div.click(() => this.describe());
+
   }
   randomVal(min, max) {
     return Math.floor(Math.random() * (max - min));
@@ -25,11 +26,19 @@ class Shape {
     $('#describe-height').val(this.height);
     $('#describe-width').val(this.width);
   }
+  getArea() {
+    let area = this.width * this.height;
+    $('#describe-area').val(area);
+  }
+  getPerimeter() {
+    let perimeter = (this.width + this.height) * 2;
+    $('#describe-perimeter').val(perimeter);
+  }
 }
 
 class Circle extends Shape {
   constructor(radius) {
-    super();
+    super(2 * radius, 2 * radius);
     this.div.attr('id', 'circle');
   }
 }
@@ -44,11 +53,18 @@ class Triangle extends Shape {
 class Rectangle extends Shape {
   constructor(width, height) {
     super(width, height)
+    this.height = Number(height);
+    this.width = Number(width);
     this.div.attr('id', 'rectangle');
     this.div.css('backgroundColor', 'green');
+    this.div.click(() => this.getArea());
+    this.div.click(() => this.getPerimeter());
+    this.div.click(() => $('#describe-shape').val('Rectangle'));
+    
   }
 }
-$('#rectangle-btn').click(function() {
+
+$('#rectangle-btn').click(function () {
   let rectWidth = $('#width-px').val();
   let rectHeight = $('#rect-height-px').val();
   new Rectangle(rectWidth, rectHeight);
@@ -58,11 +74,23 @@ $('#rectangle-btn').click(function() {
 class Square extends Shape {
   constructor(sideLength) {
     super(sideLength, sideLength);
+    this.sideLength = Number(sideLength);
     this.div.attr('id', 'square');
     this.div.css('backgrounColor', 'red');
+    this.div.click(() => this.getArea());
+    this.div.click(() => this.getPerimeter());
+    this.div.click(() => $('#describe-shape').val('Square'));
+  }
+  getArea() {
+    let area = this.sideLength * this.sideLength;
+    $('#describe-area').val(area);
+  }
+  getPerimeter() {
+    let perimeter = (this.sideLength * 2) * 2;
+    $('#describe-perimeter').val(perimeter);
   }
 }
-$('#square-btn').click(function() {
+$('#square-btn').click(function () {
   let sidePx = $('#side-px').val();
   new Square(sidePx, sidePx);
 })
